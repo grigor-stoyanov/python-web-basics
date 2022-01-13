25e6200f0eb0110420b3ed756515c3de3ea4b702
grigor-stoyanov <grigor.stoyanov@hotmail.com>
2022-01-13 13:45:17 +0200

    introduction to python web, http basics refresher lecture

---
+++ internet-and-http/livedemo.py
@@ -0,0 +1,6 @@
+# What is http & requests? see JS-advanced module
+# MIME: multipurpose email extension
+# used for encoding resources and developed for email attachments
+# used in http and smtp it allows describing media types to client
+# content-type: application/json image/png....
+# http codes: 1xx informational,2xx successful, 3xx redirectional, 4xx client error, 5xx service error
