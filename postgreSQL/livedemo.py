9c44a60886833e5d4e94b3060b1c87e61c75d5c9
grigor-stoyanov <grigor.stoyanov@hotmail.com>
2022-01-18 14:19:24 +0200

    postgre data manipulation and storage,docker and sql theory

---
+++ postgreSQL/livedemo.py
@@ -0,0 +1,23 @@
+# data management vs data storage
+# different db's have different speeds
+# mongodb better at adding, postgreSQL better at searching
+# dbms(database management system) imposes rules on contained data
+# user doesn't have direct access
+# 3 types of relations 1 to 1, 1 to many, many to many
+# one user has one profile and one profile has one user
+# One country has many Cities but one city has one country
+# one employee has many projects and one project has many employees
+# foreign key vs primary key
+# many to many e.g. employees-projects-mapping table
+# junction table adds unique primary key to mapping table
+# Structured Query Language
+# Addressing many records with 1 command
+# SQL statements have clause,and expressions
+# Data Definition - CREATE,ALTER,DROP
+# Data Manipulation - SELECT,INSERT,DELETE,UPDATE
+# Data Control - GRANT,REVOKE,DENY
+# Transaction Control - COMMIT,ROLLBACK,SAVE
+# Postgre - Relational DB
+# extensible
+# Fields can be of type char[(M)] fixed length,varchar([N]),TEXT(any length)
+# numeric data types SMALLINT,INT,BIGINT,DECIMAL,NUMERIC,REAL,DOUBLE PRECISION,SERIAL,SMALLSERIAL,BIGSERIAL
